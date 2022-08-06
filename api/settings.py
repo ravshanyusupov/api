38f069996731889ea4b69a6efd2a20838c4af468
ravshan_yusupov <yusupovravshan001@gmail.com>
2022-08-06 10:48:00 +0500

    bnrenwerbt

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#^7&(!a6x@oat-gfm$rgk3wjkd7l#uckp^hs^7g0_gf@pp8@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
